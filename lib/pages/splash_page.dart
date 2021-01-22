74ea93dba55b3fc6f505a373476f10ca0518ffb7
Rizki <rizkikurniawan1797@gmail.com>
2021-01-22 21:49:18 +0700

    Added footer with stack and handle bottom iphone

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -6,64 +6,72 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.only(
-          top: 50,
-          left: 30,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/logo.png'),
+          bottom: false,
+          child: Stack(
+            children: [
+              Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Image.asset('assets/images/splash_image.png')),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 50,
+                  left: 30,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/logo.png'),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'Cari Kos Tanpa Ribet',
+                      style: blackTextStyle.copyWith(
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Dapatkan informasi dan nikmati\nkemudahan tanpa harus keluar rumah',
+                      style: greyTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      width: 210,
+                      height: 50,
+                      child: RaisedButton(
+                        onPressed: () {},
+                        color: purpleColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                        child: Text(
+                          'Explore Now',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              'Cari Kos Tanpa Ribet',
-              style: blackTextStyle.copyWith(
-                fontSize: 24,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Dapatkan informasi dan nikmati\nkemudahan tanpa harus keluar rumah',
-              style: greyTextStyle.copyWith(
-                fontSize: 16,
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-              width: 210,
-              height: 50,
-              child: RaisedButton(
-                onPressed: () {},
-                color: purpleColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(17),
-                ),
-                child: Text(
-                  'Explore Now',
-                  style: whiteTextStyle.copyWith(
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      )),
+            ],
+          )),
     );
   }
 }
