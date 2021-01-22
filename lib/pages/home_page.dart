34b00d566ccf2c4d6d7cf7de3e80a0d8772ea438
Rizki <rizkikurniawan1797@gmail.com>
2021-01-22 22:08:29 +0700

    Modified header

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:padang_kos/theme.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text(
-          'Home Page',
+          child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: edge,
         ),
-      ),
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                left: edge,
+              ),
+              child: Text(
+                'Explore Now',
+                style: blackTextStyle.copyWith(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Temukan kosan idaman mu',
+                style: greyTextStyle.copyWith(fontSize: 16),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            )
+          ],
+        ),
+      )),
     );
   }
 }
