fe47b9a3557209eac1a77ff2da8f09c7ecbb6787
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-25 23:38:16 +0700

    Added model space

---
+++ lib/models/space.dart
@@ -0,0 +1,19 @@
+class Space {
+  int id;
+  String name;
+  String imgUrl;
+  double price;
+  String district;
+  String city;
+  int rating;
+
+  Space({
+    this.id,
+    this.name,
+    this.imgUrl,
+    this.price,
+    this.district,
+    this.city,
+    this.rating,
+  });
+}
