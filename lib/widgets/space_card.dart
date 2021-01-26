fdca5c815859c7a0c71f46743e5c7d9b35fa211a
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-26 21:14:15 +0700

    Updated image from network

--- lib/widgets/space_card.dart
+++ lib/widgets/space_card.dart
@@ -28,8 +28,11 @@ class SpaceCard extends StatelessWidget {
               height: 110,
               child: Stack(
                 children: [
-                  Image.asset(
+                  Image.network(
                     space.imgUrl,
+                    width: 130,
+                    height: 110,
+                    fit: BoxFit.cover,
                   ),
                   Align(
                     alignment: Alignment.topRight,
