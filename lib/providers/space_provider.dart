140ff5f3be0d187b38b4b9b9de61b3e3f07ea7d2
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-26 21:14:34 +0700

    Send data

--- lib/providers/space_provider.dart
+++ lib/providers/space_provider.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:padang_kos/models/space.dart';
 
 class SpaceProvider extends ChangeNotifier {
   getRecommendedSpace() async {
@@ -7,5 +10,13 @@ class SpaceProvider extends ChangeNotifier {
         await http.get('http://bwa-cozy.herokuapp.com/recommended-spaces');
     print(result.statusCode);
     print(result.body);
+
+    if (result.statusCode == 200) {
+      List data = jsonDecode(result.body);
+      List<Space> spaces = data.map((item) => Space.fromJson(item)).toList();
+      return spaces;
+    } else {
+      return <Space>[];
+    }
   }
 }
