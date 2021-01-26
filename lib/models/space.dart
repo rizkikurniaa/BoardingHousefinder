ac3b4ed6c3e45f8c8a57ff8c17d2d979eec72745
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-26 21:34:35 +0700

    Updated with photos

--- lib/models/space.dart
+++ lib/models/space.dart
@@ -42,6 +42,7 @@ class Space {
     address = json['address'];
     phone = json['phone'];
     mapUrl = json['map_url'];
+    photos = json['photos'];
     numberOfKitchens = json['number_of_kitchens'];
     numberOfBedrooms = json['number_of_bedrooms'];
     numberOfCupboards = json['number_of_cupboards'];
