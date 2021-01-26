572b16d7393ffec992771755c246c7a0592a97bd
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-26 11:39:00 +0700

    Updated with navigator push

--- lib/widgets/space_card.dart
+++ lib/widgets/space_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:padang_kos/models/space.dart';
+import 'package:padang_kos/pages/detail_page.dart';
 
 import '../theme.dart';
 
@@ -9,92 +10,102 @@ class SpaceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(18),
-          child: Container(
-            width: 130,
-            height: 110,
-            child: Stack(
-              children: [
-                Image.asset(
-                  space.imgUrl,
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    width: 70,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      color: purpleColor,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(36),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailPage(),
+          ),
+        );
+      },
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: Container(
+              width: 130,
+              height: 110,
+              child: Stack(
+                children: [
+                  Image.asset(
+                    space.imgUrl,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 70,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: purpleColor,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(36),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/icon_star.png',
+                            width: 22,
+                            height: 22,
+                          ),
+                          Text(
+                            '${space.rating}/5',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/images/icon_star.png',
-                          width: 22,
-                          height: 22,
-                        ),
-                        Text(
-                          '${space.rating}/5',
-                          style: whiteTextStyle.copyWith(
-                            fontSize: 13,
-                          ),
-                        ),
-                      ],
-                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              space.name,
-              style: blackTextStyle.copyWith(
-                fontSize: 18,
-              ),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Text.rich(
-              TextSpan(
-                text: '\$${space.price}',
-                style: purpleTextStyle.copyWith(
-                  fontSize: 16,
-                ),
-                children: [
-                  TextSpan(
-                      text: '/month',
-                      style: greyTextStyle.copyWith(
-                        fontSize: 16,
-                      )),
                 ],
               ),
             ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              '${space.district}, ${space.city}',
-              style: greyTextStyle,
-            )
-          ],
-        )
-      ],
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                space.name,
+                style: blackTextStyle.copyWith(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text.rich(
+                TextSpan(
+                  text: '\$${space.price}',
+                  style: purpleTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  children: [
+                    TextSpan(
+                        text: '/month',
+                        style: greyTextStyle.copyWith(
+                          fontSize: 16,
+                        )),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                '${space.district}, ${space.city}',
+                style: greyTextStyle,
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
