76ee811df8cf58db8d8d58bed5fd66cc1eb22901
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-26 10:17:23 +0700

    Implemented bottom navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,7 @@ import 'package:padang_kos/models/districts.dart';
 import 'package:padang_kos/models/space.dart';
 import 'package:padang_kos/models/tips.dart';
 import 'package:padang_kos/theme.dart';
+import 'package:padang_kos/widgets/bottom_navbar_item.dart';
 import 'package:padang_kos/widgets/districts_card.dart';
 import 'package:padang_kos/widgets/space_card.dart';
 import 'package:padang_kos/widgets/tips_card.dart';
@@ -14,193 +15,230 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          bottom: false,
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: edge,
-            ),
-            child: ListView(
-              children: [
-                // NOTE : TITLE/HEADER
-                Padding(
-                  padding: EdgeInsets.only(
-                    left: edge,
+        bottom: false,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: edge,
+          ),
+          child: ListView(
+            children: [
+              // NOTE : TITLE/HEADER
+              Padding(
+                padding: EdgeInsets.only(
+                  left: edge,
+                ),
+                child: Text(
+                  'Explore Now',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 24,
                   ),
-                  child: Text(
-                    'Explore Now',
-                    style: blackTextStyle.copyWith(
-                      fontSize: 24,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: edge),
+                child: Text(
+                  'Temukan kosan idaman mu',
+                  style: greyTextStyle.copyWith(fontSize: 16),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              // NOTE : POPULAR DISTRICTS
+              Padding(
+                padding: EdgeInsets.only(left: edge),
+                child: Text(
+                  'Popular Districts',
+                  style: regulerTextStyle.copyWith(fontSize: 16),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    SizedBox(
+                      width: 24,
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: 2,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: edge),
-                  child: Text(
-                    'Temukan kosan idaman mu',
-                    style: greyTextStyle.copyWith(fontSize: 16),
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                // NOTE : POPULAR DISTRICTS
-                Padding(
-                  padding: EdgeInsets.only(left: edge),
-                  child: Text(
-                    'Popular Districts',
-                    style: regulerTextStyle.copyWith(fontSize: 16),
-                  ),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Container(
-                  height: 150,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      SizedBox(
-                        width: 24,
-                      ),
-                      DistrictsCard(
-                        Districts(
-                            id: 1,
-                            name: 'Kuranji',
-                            imageUrl: 'assets/images/city1.png'),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      DistrictsCard(
-                        Districts(
-                            id: 2,
-                            name: 'Pauh',
-                            imageUrl: 'assets/images/city2.png',
-                            isPopular: true),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      DistrictsCard(
-                        Districts(
-                            id: 3,
-                            name: 'Lubeg',
-                            imageUrl: 'assets/images/city3.png'),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                // NOTE : RECOMMENDED SPACE
-                Padding(
-                  padding: EdgeInsets.only(left: edge),
-                  child: Text(
-                    'Recommended Space',
-                    style: regulerTextStyle.copyWith(fontSize: 16),
-                  ),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: edge),
-                  child: Column(
-                    children: [
-                      SpaceCard(
-                        Space(
+                    DistrictsCard(
+                      Districts(
                           id: 1,
-                          name: 'Kos Rizki',
-                          imgUrl: 'assets/images/space1.png',
-                          price: 52,
-                          district: 'Kuranji',
-                          city: 'Padang',
-                          rating: 4,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      SpaceCard(
-                        Space(
+                          name: 'Kuranji',
+                          imageUrl: 'assets/images/city1.png'),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    DistrictsCard(
+                      Districts(
                           id: 2,
-                          name: 'Kos Kikulabs',
-                          imgUrl: 'assets/images/space2.png',
-                          price: 80,
-                          district: 'Padang Timur',
-                          city: 'Padang',
-                          rating: 3,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      SpaceCard(
-                        Space(
+                          name: 'Pauh',
+                          imageUrl: 'assets/images/city2.png',
+                          isPopular: true),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    DistrictsCard(
+                      Districts(
                           id: 3,
-                          name: 'Kos Sulistyo',
-                          imgUrl: 'assets/images/space3.png',
-                          price: 32,
-                          district: 'Pauh',
-                          city: 'Padang',
-                          rating: 3,
-                        ),
+                          name: 'Lubeg',
+                          imageUrl: 'assets/images/city3.png'),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              // NOTE : RECOMMENDED SPACE
+              Padding(
+                padding: EdgeInsets.only(left: edge),
+                child: Text(
+                  'Recommended Space',
+                  style: regulerTextStyle.copyWith(fontSize: 16),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: edge),
+                child: Column(
+                  children: [
+                    SpaceCard(
+                      Space(
+                        id: 1,
+                        name: 'Kos Rizki',
+                        imgUrl: 'assets/images/space1.png',
+                        price: 52,
+                        district: 'Kuranji',
+                        city: 'Padang',
+                        rating: 4,
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                // NOTE : TIPS & GUIDANCE
-                Padding(
-                  padding: EdgeInsets.only(left: edge),
-                  child: Text(
-                    'Tips & Guidance',
-                    style: regulerTextStyle.copyWith(fontSize: 16),
-                  ),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: edge,
-                  ),
-                  child: Column(
-                    children: [
-                      TipsCard(
-                        Tips(
-                          id: 1,
-                          title: 'City Guidelines',
-                          imgUrl: 'assets/images/tips1.png',
-                          updatedAt: '26 Jan',
-                        ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    SpaceCard(
+                      Space(
+                        id: 2,
+                        name: 'Kos Kikulabs',
+                        imgUrl: 'assets/images/space2.png',
+                        price: 80,
+                        district: 'Padang Timur',
+                        city: 'Padang',
+                        rating: 3,
                       ),
-                      SizedBox(
-                        height: 20,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    SpaceCard(
+                      Space(
+                        id: 3,
+                        name: 'Kos Sulistyo',
+                        imgUrl: 'assets/images/space3.png',
+                        price: 32,
+                        district: 'Pauh',
+                        city: 'Padang',
+                        rating: 3,
                       ),
-                      TipsCard(
-                        Tips(
-                          id: 2,
-                          title: 'Padang Fairship',
-                          imgUrl: 'assets/images/tips2.png',
-                          updatedAt: '17 Dec',
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              // NOTE : TIPS & GUIDANCE
+              Padding(
+                padding: EdgeInsets.only(left: edge),
+                child: Text(
+                  'Tips & Guidance',
+                  style: regulerTextStyle.copyWith(fontSize: 16),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: edge,
+                ),
+                child: Column(
+                  children: [
+                    TipsCard(
+                      Tips(
+                        id: 1,
+                        title: 'City Guidelines',
+                        imgUrl: 'assets/images/tips1.png',
+                        updatedAt: '26 Jan',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TipsCard(
+                      Tips(
+                        id: 2,
+                        title: 'Padang Fairship',
+                        imgUrl: 'assets/images/tips2.png',
+                        updatedAt: '17 Dec',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Container(
+        height: 65,
+        width: MediaQuery.of(context).size.width - (2 * edge),
+        margin: EdgeInsets.symmetric(
+          horizontal: edge,
+        ),
+        decoration: BoxDecoration(
+          color: Color(0xffF6F7F8),
+          borderRadius: BorderRadius.circular(23),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            BottomNavbarItem(
+              imgUrl: 'assets/images/icon_home.png',
+              isActive: true,
             ),
-          )),
+            BottomNavbarItem(
+              imgUrl: 'assets/images/icon_mail.png',
+              isActive: false,
+            ),
+            BottomNavbarItem(
+              imgUrl: 'assets/images/icon_card.png',
+              isActive: false,
+            ),
+            BottomNavbarItem(
+              imgUrl: 'assets/images/icon_love.png',
+              isActive: false,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
